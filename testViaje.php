12b903758ab85c297d3e355abdf82f94319a58ef
gabigallardo <129552904+gabigallardo@users.noreply.github.com>
2023-06-11 16:05:09 -0300

    Update testViaje.php

--- testViaje.php
+++ testViaje.php
@@ -1,77 +1,77 @@
 <?php
-require_once 'Viaje.php';
+include "Viaje.php";
+$viaje = new Viaje("V001", "Bariloche", 50);
 
-$viaje = new Viaje("VF1", "San Martin", 100);
-
-while (true) {
-    echo "Ingrese una opción:\n";
-    echo "1) Ver datos del viaje\n";
-    echo "2) Modificar código del viaje\n";
-    echo "3) Modificar destino de viaje\n";
-    echo "4) Modificar cantidad máxima de pasajeros\n";
-    echo "5) Agregar pasajero\n";
-    echo "6) Eliminar pasajero\n";
-    echo "0) Salir\n";
-    
-    $opcion = readline();
+do {
+    echo "===== Menú =====" . " \n";
+    echo "1. Cargar información del viaje" . " \n";
+    echo "2. Modificar información de los pasajeros" . " \n";
+    echo "3. Ver información del viaje" . " \n";
+    echo "4. Eliminar pasajeros \n";
+    echo "5. Agregar Pasajeros \n";
+    echo "6. Salir" . " \n";
+    echo "================" . " \n";
+    $opcion = readline("Ingrese una opción: ");
 
     switch ($opcion) {
-        case "1":
-            echo "Código del viaje: " . $viaje->getCodigo() . "\n";
-            echo "Destino: " . $viaje->getDestino() . "\n";
-            echo "Cantidad máxima de pasajeros: " . $viaje->getMaxPasajeros() . "\n";
-            echo "Pasajeros:\n";
-            $pasajeros = $viaje->getPasajeros();
-            foreach ($pasajeros as $pasajero) {
-                echo $pasajero["nombre"] . " " . $pasajero["apellido"] . " (" . $pasajero["numDoc"] . ")\n";
-            }
-            break;
-        case "2":
-            echo "Ingrese el nuevo código del viaje:\n";
-            $codigo = readline();
+        case '1':
+            $codigo = readline("Ingrese el código del viaje: ");
+            $destino = readline("Ingrese el destino del viaje: ");
+            $cantidadMaxPasajeros = readline("Ingrese la cantidad máxima de pasajeros: ");
             $viaje->setCodigo($codigo);
-            echo "Código modificado con éxito\n";
-            break;
-        case "3":
-            echo "Ingrese el nuevo destino del viaje:\n";
-            $destino = readline();
             $viaje->setDestino($destino);
-            echo "Destino modificado con éxito\n";
+            $viaje->setCantidadMaxPasajeros($cantidadMaxPasajeros);
+            echo "Información del viaje cargada exitosamente." . " \n";
+            echo " \n";
             break;
-        case "4":
-            echo "Ingrese la nueva cantidad máxima de pasajeros:\n";
-            $maxPasajeros = readline();
-            $viaje->setMaxPasajeros($maxPasajeros);
-            echo "Cantidad máxima de pasajeros modificada con éxito\n";
+
+        case '2':
+            echo "Pasajeros actuales: " . " \n";
+            $pasajeros = $viaje->getPasajeros();
+            foreach ($pasajeros as $indice => $pasajero) {
+                echo "Pasajero " . ($indice + 1) . ": " . $pasajero['nombre'] . " " . $pasajero['apellido'] . " \n";
+            }
+            echo " \n";
+            $numeroDoc = readline("Ingrese el documento del pasajero a modificar: ");
+            $viaje->modificarPasajero($numeroDoc);
+            echo " \n";
             break;
-        case "5":
-            echo "Ingrese el nombre del pasajero:\n";
-            $nombre = readline();
-            echo "Ingrese el apellido del pasajero:\n";
-            $apellido = readline();
-            echo "Ingrese el número de documento del pasajero:\n";
-            $numDoc = readline();
-            if ($viaje->agregarPasajero($nombre, $apellido, $numDoc)) {
-                echo "Pasajero agregado con éxito\n";
-            } else {
-                echo "No se pudo agregar al pasajero. El límite de pasajeros ha sido alcanzado.\n";
+
+        case '3':
+            echo "Información del viaje: " . " \n";
+            $viaje->mostrarInformacion();
+            echo " \n";
+            break;
+        case '4':
+            echo "Ingrese el numero de documento del pasajero a eliminar: \n";
+            $numDoc = trim(fgets(STDIN));
+            if($viaje->eliminarPasajero($numDoc)){
+                echo "Pasajero Eliminado Correctamente \n";
+            }else {
+                echo "El Pasajero no ha sido eliminado \n";
+            }
+                break;               
+        case '5':
+            echo "Ingrese el nombre del nuevo Pasajero: \n";
+            $nombre = trim(fgets(STDIN)) ;
+            echo "Ingrese el apellido del nuevo Pasajero: \n";
+            $apellido = trim(fgets(STDIN)); 
+            echo "Ingrese el numero de documento del nuevo Pasajero: \n";
+            $numeroDocumento = trim(fgets(STDIN));
+            if ( $viaje->agregarPasajeros($nombre,$apellido,$numeroDocumento)) {
+                echo "Pasajero agregado correctamente \n";
+            }else {
+                echo "Cantidad maxima de pasajeros superada \n" ;
             }
             break;
-        case "6":
-            echo "Ingrese el número de documento del pasajero a eliminar:\n";
-            $numDoc = readline();
-            if ($viaje->eliminarPasajero($numDoc)) {
-                echo "Pasajero eliminado con éxito\n";
-            } else {
-                echo "No se pudo eliminar al pasajero. El pasajero no se encontró en la lista.\n";
-            }
+            
+        case '6':
+            echo "Saliendo..." . " \n";
             break;
-        case "0":
-            echo "Adiós!\n";
-            exit();
+
         default:
-            echo "Opción inválida. Intente nuevamente\n";
+            echo "Opción inválida. Por favor, ingrese una opción válida." . " \n";
+            echo " \n";
             break;
     }
-}
-
+} while ($opcion != '6');
